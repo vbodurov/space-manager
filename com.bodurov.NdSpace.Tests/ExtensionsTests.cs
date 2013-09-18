b7f1441215d082cdb732390d3b01ba72da6ed487
vbodurov <vladimir@bodurov.com>
2013-09-17 20:47:33 -0700

    temp

--- com.bodurov.NdSpace.Tests/ExtensionsTests.cs
+++ com.bodurov.NdSpace.Tests/ExtensionsTests.cs
@@ -1,4 +1,6 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
 using com.bodurov.NdSpace.Extensions;
 
 namespace YouVisio.Unity3D.AI.Tests
@@ -20,15 +22,47 @@ namespace YouVisio.Unity3D.AI.Tests
              Assert.That(a.IsEqualTo(b), Is.EqualTo(expect));
         }
 
-//        [Test]
-//        public void T()
-//        {
-//            var n = 100000.0;
-//            for (var i = 0; i < 40; ++i)
-//            {
-//                n *= 1.06;
-//                Console.WriteLine(i + "=" + n);
-//            }   
-//        }
+        [Test]
+        public void T()
+        {
+            var arr1 = @"
+FancyBox logo1.jpg
+FancyBox logo2-01.jpg
+FancyBox logo3-01.jpg
+FancyBox website1-01.jpg
+FancyBox website2-01.jpg
+FancyBox website3-01.jpg
+FancyBox logo1.jpg
+FancyBox logo2-01.jpg
+FancyBox logo3-01.jpg
+FancyBox website1-01.jpg
+FancyBox website2-01.jpg
+FancyBox website3-01.jpg".Split('\n').Select(s => s.Trim()).ToArray();
+            var arr2 = @"
+Tile 150 logo1.jpg
+Tile 150 logo2-01.jpg
+Tile 150 logo3-01.jpg
+Tile 150 website1-01.jpg
+Tile 150 website2-01.jpg
+Tile 150 website3-01.jpg
+Tile 150 logo1.jpg
+Tile 150 logo2-01.jpg
+Tile 150 logo3-01.jpg
+Tile 150 website1-01.jpg
+Tile 150 website2-01.jpg
+Tile 150 website3-01.jpg".Split('\n').Select(s => s.Trim()).ToArray();
+
+            for (var i = 0; i < arr1.Length; ++i)
+            {
+                Console.WriteLine(@"
+                <div class=""tile"">
+                    <a class=""fancybox"" href=""images/{0}"">
+                        <img src=""images/{1}"" alt=""""/>
+                    </a>
+                </div>", arr1[i], arr2[i]);
+            }
+
+                
+        }
     }
 }
